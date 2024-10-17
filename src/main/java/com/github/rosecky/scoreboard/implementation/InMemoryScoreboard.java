6dd1b5657ee51f1fa1e68b1d6f7e9c76fd3370e3
rosecky <jan.rosecky@epimorphics.com>
2024-10-17 16:10:22 +0200

    InMemoryScoreboard implementation, no input order taken into account

--- src/main/java/com/github/rosecky/scoreboard/implementation/InMemoryScoreboard.java
+++ src/main/java/com/github/rosecky/scoreboard/implementation/InMemoryScoreboard.java
@@ -4,30 +4,51 @@ import com.github.rosecky.scoreboard.model.GameState;
 import com.github.rosecky.scoreboard.model.Score;
 import com.github.rosecky.scoreboard.model.Team;
 import com.github.rosecky.scoreboard.repository.Scoreboard;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
-import java.util.List;
+import java.util.*;
 
 public class InMemoryScoreboard implements Scoreboard {
 
-
+    private final Map<ImmutablePair<Team, Team>, GameState> gamesInProgress = new HashMap<>();
 
     @Override
     public void trackGameState(GameState gameState) {
-
+        var key = new ImmutablePair<>(gameState.getHomeTeam(), gameState.getAwayTeam());
+        if (gamesInProgress.containsKey(key)) {
+            throw new IllegalArgumentException("Game between %s and %s already in progress".formatted(gameState.getHomeTeam(), gameState.getAwayTeam()));
+        }
+        gamesInProgress.put(key, gameState);
     }
 
     @Override
     public GameState updateScore(Team homeTeam, Team awayTeam, Score newScore) {
-        return null;
+        var key = new ImmutablePair<>(homeTeam, awayTeam);
+        if (!gamesInProgress.containsKey(key)) {
+            throw new IllegalArgumentException("Game between %s and %s does not exist".formatted(homeTeam, awayTeam));
+        }
+        var newGameState = GameState.withTeamsAndScore(homeTeam, awayTeam, newScore);
+        gamesInProgress.put(key, newGameState);
+        return newGameState;
     }
 
     @Override
     public GameState finishGameBetween(Team homeTeam, Team awayTeam) {
-        return null;
+        var key = new ImmutablePair<>(homeTeam, awayTeam);
+        if (!gamesInProgress.containsKey(key)) {
+            throw new IllegalArgumentException("Game between %s and %s does not exist".formatted(homeTeam, awayTeam));
+        }
+        var gameState = gamesInProgress.get(key);
+        gamesInProgress.remove(key);
+        return gameState;
     }
 
     @Override
     public List<GameState> getSummaryOfGamesInProgress() {
-        return null;
+        return gamesInProgress.values()
+                .stream()
+                .sorted(
+                        Comparator.comparing((GameState gameState) -> gameState.getScore().getTotal()).reversed()
+                ).toList();
     }
 }
